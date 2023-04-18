85ec1841e048a0e81e6bc5768a08875572d2f216
adudko <adudko@umassd.edu>
2023-04-18 13:37:34 -0400

    new methods 1

--- Program.cs
+++ Program.cs
@@ -30,13 +30,69 @@ namespace ECE264AdventureGame2023
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Cyber Conspiracy!");
+            Console.WriteLine("Welcome to Cyber Conspiracy!");
+            string playerName = WelcomePlayer();
+            int currentRoomID = 1;
 
-            Console.WriteLine("Hello taveler.");
+
+
 
 
 
 
 
         }
+
+
+
+
+
+
+
+
+
+        static string WelcomePlayer()
+        {
+            ///Insert other introductory stuff here
+            Console.WriteLine("Welcome to Cyber Conspiracy!");
+            Console.WriteLine("Please enter your name: ");            
+            string userInput = Console.ReadLine();
+            return userInput;
+
+        }
+        static bool GetYesNo(string prompt)
+        {
+            string[] valid = { "YES", "Y", "NO", "N" };
+            string ans;
+            ans = GetString(prompt, valid, "?Invalid response, please reenter");
+            return (ans == "YES" || ans == "Y");
+
+        }
+
+        //Universal get string with prompt, valid values, and error message
+        static string GetString(string prompt, string[] valid, string error)
+        {
+            //prompt = user prompt, valid = array of valid responses
+            //error = msg to display on invalid entry
+            //all strings returned upper case. all valid[] entries must be in upper case
+            string response;
+            bool OK = false;
+            do
+            {
+                Console.Write(prompt);
+                response = Console.ReadLine().ToUpper();
+                foreach (string s in valid) if (response == s.ToUpper()) OK = true;
+                if (!OK) Console.WriteLine(error);
+
+            } while (!OK);
+            return response;
+        }
+
+
+
+
+
+
     }
 }
